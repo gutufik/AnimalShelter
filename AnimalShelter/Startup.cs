a650f1e3d36158ea06c8d6aa351c11c14223517b
Михаил Мясников <gutufik@mail.ru>
2021-12-18 14:51:16 +0300

    Add new layout

--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -51,7 +51,7 @@ namespace AnimalShelter
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Account}/{action=Login}/{id?}");
             });
         }
     }
